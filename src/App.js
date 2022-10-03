3662ca65a937d6c99e2fb7b9e7b02eaa8a2f1b13
Joseph Olusegun <josepholusegun3@gmail.com>
2022-10-03 11:31:58 +0100

    todo app built with react

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import TodoList from "./components/TodoList";
+
+function App() {
+  return (
+    <div className="todo-app">
+      {/* <TodoForm /> */}
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
